11b2939deb9e25b5e160b25a65ec6fbd42881e2e
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2025-05-30 22:26:42 +1200

    set server on vite config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,8 +1,12 @@
+/// <reference types="@photonjs/core/api" />
 import { defineConfig } from "vite";
 // import react from "@vitejs/plugin-react";
 import { frameworkPlugin } from "@repo/framework-lib/vite";
 
 export default defineConfig({
+	photon: {
+		server: "./server.ts",
+	},
 	plugins: [
 		frameworkPlugin(),
 		// react()
