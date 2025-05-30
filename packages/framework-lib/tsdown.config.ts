7beb32612b2594aecc1477ea7e83453ee21df422
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2025-05-30 21:47:32 +1200

    more matching

--- packages/framework-lib/tsdown.config.ts
+++ packages/framework-lib/tsdown.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "tsdown";
 
 export default defineConfig({
 	entry: [
-		"./src/index.ts",
 		"./src/vite/index.ts",
 		"./src/photon/entries/prod.ts",
 		"./src/photon/entries/dev.ts",
