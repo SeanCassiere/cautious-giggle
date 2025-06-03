96a3b9d2b5b7565fe9d1010843eefc8a5c91087f
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2025-06-03 15:34:18 +1200

    server static assets for prod using `sirv`

--- packages/framework-lib/src/photon/entries/dev.ts
+++ packages/framework-lib/src/photon/entries/dev.ts
@@ -1,6 +1,6 @@
-import type { UniversalMiddleware } from "@universal-middleware/core";
 import { apiMiddleware } from "../middlewares/api";
 import { ssrMiddleware } from "../middlewares/ssr";
+import type { UniversalMiddleware } from "@universal-middleware/core";
 
 export default [
 	apiMiddleware,
--- packages/framework-lib/src/photon/entries/prod.ts
+++ packages/framework-lib/src/photon/entries/prod.ts
@@ -1,8 +1,11 @@
-import type { UniversalMiddleware } from "@universal-middleware/core";
+import { enhance } from "@universal-middleware/core";
+import sirv from "@universal-middleware/sirv";
 import { apiMiddleware } from "../middlewares/api";
 import { ssrMiddleware } from "../middlewares/ssr";
+import type { UniversalMiddleware } from "@universal-middleware/core";
 
 export default [
+	enhance(sirv("dist/client"), { name: "sirv" }),
 	apiMiddleware,
 	ssrMiddleware,
 ] satisfies Array<UniversalMiddleware>;
